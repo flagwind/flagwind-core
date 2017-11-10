caddc624c78ae8d4fdcbe32771cc8da0a4388c8a
jason <jasonsoop@gmail.com>
2017-11-10 17:03:37 +0800

    新增 Type 类的数组判断和单元测试

--- src/reflection/Type.ts
+++ src/reflection/Type.ts
@@ -6,17 +6,6 @@
  * Copyright (C) 2010-2017 Flagwind Inc. All rights reserved. 
  */
 
-// const toString = Object.prototype.toString;
-
-// /**
-//  * 获取类型的字符串实例。
-//  * @private
-//  */
-// const typeString = function()
-// {
-//     return toString.call(this).slice(8, -1).toLowerCase();
-// };
-
 /**
  * 提供一些常用类型检测与反射相关的方法。
  * 
@@ -40,7 +29,12 @@ export class Type
      */
     public static isArray(value: any): boolean
     {
-        return true;
-        // return typeString.call(value) === "array";
+        return this.getTypeString(value) === "array";
+    }
+
+    private static getTypeString(value: any): string
+    {
+        // tslint:disable-next-line:no-magic-numbers
+        return Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
     }
 }
--- test/unit/specs/reflection/Type.spec.ts
+++ test/unit/specs/reflection/Type.spec.ts
@@ -6,14 +6,32 @@
  * Copyright (C) 2010-2017 Flagwind Inc. All rights reserved. 
  */
 
-// import { assert } from "chai";
-// import { Type } from "../../../../src/reflection/Type";
+import { assert } from "chai";
+import { Type } from "../../../../src/reflection/Type";
 
-describe("reflection.Type", () =>
+describe("TypeTest", () =>
 {
+    const nullValue = null;
+    const undefinedValue = undefined;
+    const dynamicArray = [1, 2, 3];
+    const typedArray = new Array<string>("hello", "word");
+    const objectValue = {};
+    const stringValue = "hello word";
+    const numberValue = 1;
+    const booleanValue = true;
+    const regexValue = /^hello$/;
+    
     it("isArrayTest", () =>
     {
-        // assert.equal(10, 10);
-        // assert.isFunction(Type.isArray);
+        assert.isTrue(Type.isArray(dynamicArray));
+        assert.isTrue(Type.isArray(typedArray));
+
+        assert.isFalse(Type.isArray(nullValue));
+        assert.isFalse(Type.isArray(undefinedValue));
+        assert.isFalse(Type.isArray(objectValue));
+        assert.isFalse(Type.isArray(stringValue));
+        assert.isFalse(Type.isArray(numberValue));
+        assert.isFalse(Type.isArray(booleanValue));
+        assert.isFalse(Type.isArray(regexValue));
     })
 });
